b757a51835c58b521e26001e2fe7a6f6c58d13ce
Shawn He <shawnhe1991@gmail.com>
2023-03-29 16:33:12 -0400

    lint file

--- breedFetcher.js
+++ breedFetcher.js
@@ -4,15 +4,15 @@ const breed  = process.argv.slice(2);
 
 request(`https://api.thecatapi.com/v1/breeds/search?q=${breed[0]}`, (error, response, body) => {
   console.log('error:', error); // Print the error if one occurred
-  if(error) {
+  if (error) {
     console.log("you broke the link!");
-    return
+    return;
   }
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
   // console.log('body:', body); // Print the HTML for the Google homepage.
   const data = JSON.parse(body);
 
-if (data.length === 0) {
+  if (data.length === 0) {
     console.log("We couldn't find the breed");
   } else {
     console.log(data[0].description);
